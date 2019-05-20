b66eb77342a25a0c0839fb07b9fd37e1f8b51c33
Atgaras <goferis.e@gmail.com>
2019-05-20 18:16:45 +0300

    6 paskaita

---
+++ Funkcija gražina simbolių kiekį/Funkcija gražina simbolių kiekį/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funkcija_gražina_simbolių_kiekį
+{
+    class Program
+    {
+        private static string PaverstiGrotelem(string name)
+        {
+            string result = "";
+            for (int i = 0; i < name.Length; i++)
+            {
+                result = result + "#";
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Įveskite žodį:");
+            string name = Console.ReadLine();
+            string zodis = PaverstiGrotelem(name);
+            Console.WriteLine(zodis);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Funkcija išveda bet kokį vardą/Funkcija išveda bet kokį vardą/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funkcija_išveda_bet_kokį_vardą
+{
+    class Program
+    {
+        private static void Vardas(int kartai, string name)
+        {
+            for (int i = 0; i < kartai; i++)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string n = "Edgaras";
+            int x = 5;
+            Vardas(x, n);
+            Console.ReadLine();
+        }
+    }
+}
